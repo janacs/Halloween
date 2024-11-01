2697a437cd30bbdb07ca8cdf40a1740ab550e1fa
Janderson Sousa <jandersonsouza_phn@hotmail.com>
2024-11-01 14:21:32 +0000

    resolvendoBug

--- src/App.js
+++ src/App.js
@@ -8,11 +8,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 //BrowserRouter container que vai ativar as rotas
 //Routes container para agrupar as rotas
 //Route é cada rota individual
+//Path => é o caminho
+//To => para
 export default function App() {
   return (
     <>
       <BrowserRouter>
-        <Header></Header>
+        <Header>
+          <Routes>
+            <Route path="/" element={<Inicio />} />
+            <Route path="/sobre" element={<Sobre />} />
+            <Route path="/fantasias" element={<Fantasias />} />
+          </Routes>
+        </Header>
       </BrowserRouter>
     </>
   );
--- src/Components/Sobre/Sobre.jsx
+++ src/Components/Sobre/Sobre.jsx
@@ -2,6 +2,12 @@ export default function Sobre() {
   return (
     <section>
       <h2>Sobre</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum atque
+        ut autem, fuga tempore alias neque dicta quos quisquam quis
+        necessitatibus, itaque minus. Necessitatibus, deserunt sequi. Quaerat
+        architecto voluptatum quam!
+      </p>
     </section>
   );
 }
