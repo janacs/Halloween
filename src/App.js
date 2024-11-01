8c5aa8a3b535873a369b04872cb75d1039e4dd79
Janderson Sousa <jandersonsouza_phn@hotmail.com>
2024-11-01 15:00:14 +0000

    atualizando-components

--- src/App.js
+++ src/App.js
@@ -3,24 +3,22 @@ import Header from "./Components/Header/Header";
 import Inicio from "./Components/Inicio/Inicio";
 import Sobre from "./Components/Sobre/Sobre";
 //importar as funcionalidades das rotas:
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-//BrowserRouter container que vai ativar as rotas
-//Routes container para agrupar as rotas
+// BrowserRouter container que vai ativar as rotas
+//Routes  container para agrupas as rotas
 //Route é cada rota individual
-//Path => é o caminho
-//To => para
+//path => caminho
+// to => para
 export default function App() {
   return (
     <>
       <BrowserRouter>
-        <Header>
-          <Routes>
-            <Route path="/" element={<Inicio />} />
-            <Route path="/sobre" element={<Sobre />} />
-            <Route path="/fantasias" element={<Fantasias />} />
-          </Routes>
-        </Header>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/sobre" element={<Sobre />} />
+          <Route path="/fantasias" element={<Fantasias />} />
+        </Routes>
       </BrowserRouter>
     </>
   );
